526890320616651487be199a84b9b26dc1a14a26
OM <shreeram@OMs-MacBook-Pro.local>
2024-11-17 15:48:00 -0600

    displaying user profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Authenticator, Heading, View } from "@aws-amplify/ui-react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const CustomSignInHeader = () => (
+  <View textAlign="center" padding="1rem">
+    <Heading level={3}>Welcome to My App made by Divya Darji</Heading>
+    <p>Created by: Divya</p> {/* Your name added here */}
+  </View>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Authenticator
+      components={{
+        Header: CustomSignInHeader, // Override the default header
+      }}
+    >
+      {({ signOut, user }) => <App />}
+    </Authenticator>
+  </React.StrictMode>
+);
